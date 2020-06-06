b82f0fc1fb12ab3e8a7b83f431736dae0bf81407
razor3538 <razor3538@mail.ru>
2020-06-06 17:59:44 +0300

    first commit

---
+++ app/Http/Controllers/Api/v1/PersonController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PersonResource;
+use App\Http\Resources\PersonResourceCollection;
+use App\Person;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    /**
+     * @param Person $person
+     * @return PersonResource
+     *
+     */
+    public function show(Person $person): PersonResource
+    {
+        return new PersonResource($person);
+    }
+
+    /**
+     * @return PersonResourceCollection
+     */
+    public function index(): PersonResourceCollection
+    {
+        return new PersonResourceCollection(Person::paginate());
+    }
+
+    /**
+     * @param Request $request
+     * @return PersonResource
+     */
+    public function store(Request $request)
+    {
+        $request -> validate([
+            'first_name'    => 'required',
+            'last_name'     => 'required',
+            'email'         => 'required',
+            'phone'         => 'required',
+            'city'          => 'required',
+        ]);
+
+        $person = Person::create($request->all());
+
+        return new PersonResource($person);
+    }
+
+    /**
+     * @param Person $person
+     * @param Request $request
+     * @return PersonResource
+     */
+    public function update(Person $person, Request $request): PersonResource
+    {
+        $person->update($request->all());
+
+        return new PersonResource($person);
+    }
+
+    public function destroy(Person $person)
+    {
+        $person->delete();
+
+        return response()->json();
+    }
+}
---
+++ app/Http/Controllers/Api/v2/PersonController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api\v2;
+
+use App\Person;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\v2\PersonResource;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    public function show(Person $person): PersonResource
+    {
+
+        return new PersonResource($person);
+    }
+}
